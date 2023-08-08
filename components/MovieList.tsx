fa5672a7b5cec38c3a263f7b310000c710a9a3f8
songiang-prime <giaochu@localhost.localdomain>
2023-08-08 13:55:51 +0700

    update list movie

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -11,20 +11,18 @@ interface MovieListProps {
 
 const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
   if (isEmpty(data)) {
-    console.log(1);
+    console.log('khong co data');
     return null;
   }
-
+  console.log(' co data');
   return (
     <div className="px-4 md:px-12 mt-4 space-y-8">
       <div>
-        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold">{title}</p>
-        <div className="grid grid-col-4 gap-2 ">
-          {/* {data.map((movie) => (
-            <MovieCard key={movie.id} data={movie}>
-              movie
-            </MovieCard>
-          ))} */}
+        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-7">{title}</p>
+        <div className="grid grid-cols-4 gap-2 ">
+          {data.map((movie) => (
+            <MovieCard key={movie.id} data={movie} />
+          ))}
         </div>
       </div>
     </div>
--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -8,8 +8,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     await serverAuth(req);
-    const movies = prismadb.movie.findMany();
-    console.log('movies', movies);
+
+    const movies = await prismadb.movie.findMany();
+
     return res.status(200).json(movies);
   } catch (error) {
     console.log(error);
